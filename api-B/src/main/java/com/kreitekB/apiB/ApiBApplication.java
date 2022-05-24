894ed6301b3c5c6ac244bf0d3bda8c81ab280207
Sergio Lorente <slorentemontoro@gmail.com>
2022-05-24 11:48:49 +0200

    Initial Commit

---
+++ api-B/src/main/java/com/kreitekB/apiB/ApiBApplication.java
@@ -0,0 +1,13 @@
+package com.kreitekB.apiB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBApplication.class, args);
+	}
+
+}
---
+++ api-B/src/test/java/com/kreitekB/apiB/ApiBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kreitekB.apiB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
