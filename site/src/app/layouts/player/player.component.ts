bcd0bb701575490210daa9cb8e95f7e87f48a89a
Sergio Lorente <slorentemontoro@gmail.com>
2022-05-27 09:39:57 +0200

    Backoffice

--- site/src/app/layouts/player/player.component.ts
+++ site/src/app/layouts/player/player.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-player',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlayerComponent implements OnInit {
 
-  constructor() { }
+  name!: string
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    const entryParam: string = this.route.snapshot.paramMap.get("name")!
+
+    this.name = entryParam
   }
 
 }
