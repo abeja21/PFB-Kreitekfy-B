c29ed5ff50cb025992e28e0c3643bd96a0e44a02
Angel_21 <74176323+abeja21@users.noreply.github.com>
2022-05-27 11:23:52 +0200

    created ArtistRepository

---
+++ Api/Api/src/main/java/com/kreitekB/Api/infrastructure/persistence/ArtistRepository.java
@@ -0,0 +1,9 @@
+package com.kreitekB.Api.infrastructure.persistence;
+
+import com.kreitekB.Api.domain.entity.Artist;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ArtistRepository extends JpaRepository<Artist,Long> {
+}
