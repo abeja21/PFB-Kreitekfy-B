48a9cce5bd8741c050d3d7cc885006ebf8b0f4b0
Angel_21 <74176323+abeja21@users.noreply.github.com>
2022-05-27 11:18:43 +0200

    created StylePersistanceImpl

---
+++ Api/Api/src/main/java/com/kreitekB/Api/infrastructure/persistence/StylePersistenceImpl.java
@@ -0,0 +1,39 @@
+package com.kreitekB.Api.infrastructure.persistence;
+
+import com.kreitekB.Api.domain.entity.Style;
+import com.kreitekB.Api.domain.persistence.StylePersistence;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+@Repository
+public class StylePersistenceImpl implements StylePersistence {
+
+    private final StyleRepository styleRepository;
+
+    @Autowired
+    public StylePersistenceImpl(StyleRepository styleRepository) {
+        this.styleRepository = styleRepository;
+    }
+
+    @Override
+    public List<Style> getAllStyles() {
+        return this.styleRepository.findAll();
+    }
+
+    @Override
+    public Optional<Style> getStyleById(Long styleId) {
+        return this.styleRepository.findById(styleId);
+    }
+
+    @Override
+    public Style saveStyle(Style style) {
+        return this.styleRepository.save(style);
+    }
+
+    @Override
+    public void deleteStyle(Long styleId) {
+    this.styleRepository.deleteById(styleId);
+    }
+}
