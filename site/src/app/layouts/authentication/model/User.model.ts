8e5fba139dc54077d3f7d7c033593c6052277941
Sergio Lorente <slorentemontoro@gmail.com>
2022-05-27 10:28:08 +0200

    authentication with boolean

--- site/src/app/layouts/authentication/model/User.model.ts
+++ site/src/app/layouts/authentication/model/User.model.ts
@@ -1,8 +1,12 @@
 export class User{
 
     name: string;
+    admin: boolean;
 
-  constructor(name: string) {
+
+  constructor(name: string, admin: boolean) {
     this.name = name
+    this.admin = admin
   }
+
 }
