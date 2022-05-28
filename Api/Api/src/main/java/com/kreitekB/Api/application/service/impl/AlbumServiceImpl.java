49937b1e0892868b57cf1c8e4d670600a73976b3
Angel_21 <74176323+abeja21@users.noreply.github.com>
2022-05-28 11:18:34 +0200

    add all ServiceImpl

---
+++ Api/Api/src/main/java/com/kreitekB/Api/application/service/impl/AlbumServiceImpl.java
@@ -0,0 +1,46 @@
+package com.kreitekB.Api.application.service.impl;
+
+import com.kreitekB.Api.application.dto.AlbumDTO;
+import com.kreitekB.Api.application.mapper.AlbumMapper;
+import com.kreitekB.Api.application.service.AlbumService;
+import com.kreitekB.Api.domain.entity.Album;
+import com.kreitekB.Api.domain.persistence.AlbumPersistence;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AlbumServiceImpl implements AlbumService {
+    private final AlbumPersistence persistence;
+    private final AlbumMapper mapper;
+
+    @Autowired
+    public AlbumServiceImpl(AlbumPersistence persistence, AlbumMapper mapper) {
+        this.persistence = persistence;
+        this.mapper = mapper;
+    }
+
+    @Override
+    public List<AlbumDTO> getAllAlbums() {
+        List<Album> albums = this.persistence.getAllAlbums();
+        return this.mapper.toDto(albums);
+    }
+
+    @Override
+    public Optional<AlbumDTO> getAlbumById(Long albumId) {
+        return this.persistence.getAlbumById(albumId).map(mapper::toDto);
+    }
+
+    @Override
+    public AlbumDTO saveAlbum(AlbumDTO albumDTO) {
+        Album albumSaved = this.persistence.saveAlbum(this.mapper.toEntity(albumDTO));
+        return this.mapper.toDto(albumSaved);
+    }
+
+    @Override
+    public void deleteAlbum(Long albumId) {
+        this.persistence.deleteAlbum(albumId);
+    }
+}
