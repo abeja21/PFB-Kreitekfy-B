c452db88ab96864aab0e0884f5d774278086a982
Sergio Lorente <slorentemontoro@gmail.com>
2022-05-31 10:28:16 +0200

    song details done

--- site/src/app/entities/song-details/model/song-details.model.ts
+++ site/src/app/entities/song-details/model/song-details.model.ts
@@ -1,35 +1,37 @@
 export class SongDetails{
-    title: string;
-    singer: string;
+    name: string;
+    artistName: string;
     duration: string;
-    upload_date: Date;
+    dateLaunch: Date;
     plays: number;
-    style: string;
-    Album: string;
-    img: string;
+    styleName: string;
+    albumName: string;
+    image: string;
     Rating: number[];
 
 
+
   constructor(
-    title: string, 
-    singer: string,
+    name: string, 
+    artistName: string, 
     duration: string, 
-    upload_date: Date, 
+    dateLaunch: Date, 
     plays: number, 
-    style: string,
-    Album: string,
-    img: string,
+    styleName: string, 
+    albumName: string, 
+    image: string, 
     Rating: number[]
 ) {
-    this.title = title
-    this.singer = singer
+    this.name = name
+    this.artistName = artistName
     this.duration = duration
-    this.upload_date = upload_date
+    this.dateLaunch = dateLaunch
     this.plays = plays
-    this.style = style
-    this.Album = Album
-    this.img = img
+    this.styleName = styleName
+    this.albumName = albumName
+    this.image = image
     this.Rating = Rating
   }
+
 
 }
