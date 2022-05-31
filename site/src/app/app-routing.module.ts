1221eda20669c9102a164590c00b079e51de5a2d
Sergio Lorente <slorentemontoro@gmail.com>
2022-05-31 18:32:00 +0200

    song form

--- site/src/app/app-routing.module.ts
+++ site/src/app/app-routing.module.ts
@@ -3,13 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { SongDetailsComponent } from './entities/song-details/song-details.component';
 import { AuthenticationComponent } from './layouts/authentication/authentication.component';
 import { BackofficeComponent } from './layouts/backoffice/backoffice.component';
+import { SongFormComponent } from './layouts/backoffice/songs/song-form/song-form.component';
 import { PlayerComponent } from './layouts/player/player.component';
 
 const routes: Routes = [
   {path: '', component: AuthenticationComponent},
   {path: 'player/:name', component: PlayerComponent},
   {path: 'admin', component: BackofficeComponent},
-  {path: 'song/:id', component: SongDetailsComponent}
+  {path: 'song/:id', component: SongDetailsComponent},
+  {path: 'admin/songEdit/:id', component: SongFormComponent}
 ];
 
 @NgModule({
